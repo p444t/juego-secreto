482927892804c84bf91f8fb087680f785498808e
p444t <patricksuazofarro@gmail.com>
2024-07-03 02:02:06 -0500

    Juego descomitteado en el js

--- app.js
+++ app.js
@@ -1,82 +1,82 @@
-// let numeroSecreto = 0;
-// let intentos = 0;
-// let listaNumerosSorteados = [];
-// let numeroMaximo = 10;
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
 
-// function asignarTextoElemento(elemento, texto) {
-//     let elementoHTML = document.querySelector(elemento);
-//     elementoHTML.innerHTML = texto;
-//     return;
-// }
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
 
 
-// function verificarIntento() {
-//     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
 
-//     // console.log(intentos);
-//     if (numeroDeUsuario == numeroSecreto) {
-//         asignarTextoElemento('p', `Acertaste el numero en ${intentos} ${(intentos == 1) ? 'vez':'veces'}`);
-//         document.getElementById('reiniciar').removeAttribute('disabled');
-//     } else {
-//         // El usuario no acerto
-//         if (numeroDeUsuario > numeroSecreto) {
-//             asignarTextoElemento('p','El numero secreto es menor');
-//         } else {
-//             asignarTextoElemento('p','El numero secreto es mayor');
-//         }
-//         intentos++;
-//         limpiarCaja();
-//     }
-//     return;
+    // console.log(intentos);
+    if (numeroDeUsuario == numeroSecreto) {
+        asignarTextoElemento('p', `Acertaste el numero en ${intentos} ${(intentos == 1) ? 'vez':'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        // El usuario no acerto
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El numero secreto es menor');
+        } else {
+            asignarTextoElemento('p','El numero secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
 
-// }
+}
 
-// function limpiarCaja() {
-//    // let valorCaja = document.querySelector('#valorUsuario');
-//    // valorCaja.value = '';   
-//    document.querySelector('#valorUsuario').value = ''; 
-// }
+function limpiarCaja() {
+   // let valorCaja = document.querySelector('#valorUsuario');
+   // valorCaja.value = '';   
+   document.querySelector('#valorUsuario').value = ''; 
+}
 
-// function generarNumeroSecreto() {
-//     let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random() * numeroMaximo) + 1;
 
-//     console.log(numeroGenerado);
-//     console.log(listaNumerosSorteados);
-//     // Si y sortemos todo los numeros
-//     if (listaNumerosSorteados.length == numeroMaximo) {
-//         asignarTextoElemento('p','Ya se sortearon todos los numeros posibles');
-//     } else {
-//     // Si el numero generado est incluido en la list
-//         if (listaNumerosSorteados.includes(numeroGenerado)) {
-//             return generarNumeroSecreto();
-//         }else {
-//             listaNumerosSorteados.push(numeroGenerado);
-//             return numeroGenerado;
-//         }
-//     }
-// }
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    // Si y sortemos todo los numeros
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los numeros posibles');
+    } else {
+    // Si el numero generado est incluido en la list
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        }else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
 
-// function condicionesIniciales() {
-//     asignarTextoElemento('h1', 'Juego del numero secreto');
-//     asignarTextoElemento('p', `Indica un numero del 1 al ${numeroMaximo}`);
-//     numeroSecreto = generarNumeroSecreto();
-//     intentos = 1;
-// }
+function condicionesIniciales() {
+    asignarTextoElemento('h1', 'Juego del numero secreto');
+    asignarTextoElemento('p', `Indica un numero del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
 
-// function reiniciarJuego(){
-//     // Limpiar la caja
-//     limpiarCaja();
-//     // indicar mensaje intervalo de numeros
-//     // generar el numero aletorio
-//     // Inicializar el numero de intentos
-//     condicionesIniciales();
-//     // deshabilitar el nuevo juego
-//     document.querySelector('#reiniciar').setAttribute('disabled','true');
+function reiniciarJuego(){
+    // Limpiar la caja
+    limpiarCaja();
+    // indicar mensaje intervalo de numeros
+    // generar el numero aletorio
+    // Inicializar el numero de intentos
+    condicionesIniciales();
+    // deshabilitar el nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
 
-// }
+}
 
-// condicionesIniciales();
+condicionesIniciales();
 
 //************************************************************************************* */
 
@@ -285,167 +285,167 @@
 //     } 
 // }
 
-// tablaMultiplicar(4);
+// tablaMultiplicar(4); @@#$%^^&**
 
-// Crea una lista vacía llamada "listaGenerica".
+// // Crea una lista vacía llamada "listaGenerica".
 
-let listaGenericaX = {};
-console.log(listaGenericaX);
+// let listaGenericaX = {};
+// console.log(listaGenericaX);
 
-// Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los
-// siguientes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
+// // Crea una lista de lenguajes de programación llamada "lenguagesDeProgramacion con los
+// // siguientes elementos: 'JavaScript', 'C', 'C++', 'Kotlin' y 'Python'.
 
-let lenguagesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
-console.log(lenguagesDeProgramacion);
+// let lenguagesDeProgramacion = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+// console.log(lenguagesDeProgramacion);
 
-// Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 'Java', 'Ruby' y 'GoLang'.
+// // Agrega a la lista "lenguagesDeProgramacion los siguientes elementos: 'Java', 'Ruby' y 'GoLang'.
 
-lenguagesDeProgramacion.push( 'Java', 'Ruby', 'GoLang');
-console.log(lenguagesDeProgramacion);
+// lenguagesDeProgramacion.push( 'Java', 'Ruby', 'GoLang');
+// console.log(lenguagesDeProgramacion);
 
-// Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion.
+// // Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion.
 
-function mostrandoElementos() {
-    console.log(lenguagesDeProgramacion);
-}
+// function mostrandoElementos() {
+//     console.log(lenguagesDeProgramacion);
+// }
 
-mostrandoElementos();
+// mostrandoElementos();
 
-// Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion
-// en orden inverso.
-
-
-
-function mostrandoInversoElementos() {
-    let indice = lenguagesDeProgramacion.length - 1;
-    while (indice >= 0) {
-        console.log(lenguagesDeProgramacion[indice]);
-        indice--;
-    }
-}
-
-mostrandoInversoElementos();
-
-// Crea una función que calcule el promedio de los elementos en una lista de números.
-
-let numerosLista = [4, 20, 10];
-function promedioLista(){
-    let promedio = 0;
-    let indice = 0;
-    while(indice < numerosLista.length){
-        console.log('Entrando al while');
-        promedio = promedio + numerosLista[indice];
-        indice++;
-    }
-    promedio = (promedio / numerosLista.length);
-    console.log(`El promedio es ${promedio}`);
-}
-
-promedioLista();
-
-// Crea una función que muestre en la consola el número más grande y el número más pequeño en una lista.
-
-let listaNumerosAleatorios = [1, 20, 18, 34, 21, 12, 25, 4, 9, 14];
-function numeroGrandePequeño(listaNumerosAleatorios) {
-
-    let min = listaNumerosAleatorios[0];
-    let max = listaNumerosAleatorios[0];
-    let indice = 1;
-
-    while(indice < listaNumerosAleatorios.length){
-        if (listaNumerosAleatorios[indice] < min) {
-            min = listaNumerosAleatorios[indice];
-        } else if (listaNumerosAleatorios[indice] > max) {
-            max = listaNumerosAleatorios[indice];
-        }
-        indice++;
-    }
-    console.log(`El numero menor es ${min}`);
-    console.log(`El numero mayor es ${max}`);
-}
-
-numeroGrandePequeño(listaNumerosAleatorios);
-
-// Crea una función que devuelva la suma de todos los elementos en una lista.
-
-function sumaElementos(lista) {
-    let suma = 0;
-    let indice = 0;
-
-    while (indice < lista.length) {
-        suma = suma + lista[indice];
-        indice++;
-    }
-    return suma;
-}
-
-let lista = [1, 3, 5, 7, 9, 20];
-let resultado = sumaElementos(lista);
-
-console.log(`La suma es ${resultado}`);
-
-// Crea una función que devuelva la posición en la lista donde se encuentra un 
-// elemento pasado como parámetro, o -1 si no existe en la lista.
-
-function encontrarPosicion(lista, elemento) {
-    let indice = 0;
-
-    while (indice < lista.length) {
-        if (lista[indice] === elemento) {
-            return indice;
-        }
-        indice++;
-    }
-
-    return -1;
-}
-
-let numerosx = [3, 5, 1, 9, 2, 8];
-let elemento = 9;
-let posicion = encontrarPosicion(numerosx, elemento);
-console.log("La posición del elemento es:", posicion);
-
-// Crea una función que reciba dos listas de números del mismo tamaño y
-// devuelva una nueva lista con la suma de los elementos uno a uno.
-
-function sumarListas(lista1, lista2) {
-    if (lista1.length !== lista2.length) {
-        console.log("Las listas no tienen el mismo tamaño.");
-        return [];
-    }
-
-    let resultado = [];
-    let i = 0;
-
-    while (i < lista1.length) {
-        resultado.push(lista1[i] + lista2[i]);
-        i++;
-    }
-
-    return resultado;
-}
-
-const lista1 = [1, 2, 3, 4];
-const lista2 = [5, 6, 7, 8];
-const sumaListas = sumarListas(lista1, lista2);
-console.log("La lista resultante es:", sumaListas);
-
-// Crea una función que reciba una lista de números y devuelva una nueva 
-// lista con el cuadrado de cada número.
-
-function cuadradoDeElementos(lista) {
-    let resultado = [];
-    let i = 0;
-
-    while (i < lista.length) {
-        resultado.push(lista[i] ** 2);
-        i++;
-    }
-
-    return resultado;
-}
+// // Crea una función que muestre en la consola todos los elementos de la lista "lenguagesDeProgramacion
+// // en orden inverso.
 
 
-const numeros = [1, 2, 3, 4, 5];
-const cuadrados = cuadradoDeElementos(numeros);
-console.log("La lista de cuadrados es:", cuadrados);
+
+// function mostrandoInversoElementos() {
+//     let indice = lenguagesDeProgramacion.length - 1;
+//     while (indice >= 0) {
+//         console.log(lenguagesDeProgramacion[indice]);
+//         indice--;
+//     }
+// }
+
+// mostrandoInversoElementos();
+
+// // Crea una función que calcule el promedio de los elementos en una lista de números.
+
+// let numerosLista = [4, 20, 10];
+// function promedioLista(){
+//     let promedio = 0;
+//     let indice = 0;
+//     while(indice < numerosLista.length){
+//         console.log('Entrando al while');
+//         promedio = promedio + numerosLista[indice];
+//         indice++;
+//     }
+//     promedio = (promedio / numerosLista.length);
+//     console.log(`El promedio es ${promedio}`);
+// }
+
+// promedioLista();
+
+// // Crea una función que muestre en la consola el número más grande y el número más pequeño en una lista.
+
+// let listaNumerosAleatorios = [1, 20, 18, 34, 21, 12, 25, 4, 9, 14];
+// function numeroGrandePequeño(listaNumerosAleatorios) {
+
+//     let min = listaNumerosAleatorios[0];
+//     let max = listaNumerosAleatorios[0];
+//     let indice = 1;
+
+//     while(indice < listaNumerosAleatorios.length){
+//         if (listaNumerosAleatorios[indice] < min) {
+//             min = listaNumerosAleatorios[indice];
+//         } else if (listaNumerosAleatorios[indice] > max) {
+//             max = listaNumerosAleatorios[indice];
+//         }
+//         indice++;
+//     }
+//     console.log(`El numero menor es ${min}`);
+//     console.log(`El numero mayor es ${max}`);
+// }
+
+// numeroGrandePequeño(listaNumerosAleatorios);
+
+// // Crea una función que devuelva la suma de todos los elementos en una lista.
+
+// function sumaElementos(lista) {
+//     let suma = 0;
+//     let indice = 0;
+
+//     while (indice < lista.length) {
+//         suma = suma + lista[indice];
+//         indice++;
+//     }
+//     return suma;
+// }
+
+// let lista = [1, 3, 5, 7, 9, 20];
+// let resultado = sumaElementos(lista);
+
+// console.log(`La suma es ${resultado}`);
+
+// // Crea una función que devuelva la posición en la lista donde se encuentra un 
+// // elemento pasado como parámetro, o -1 si no existe en la lista.
+
+// function encontrarPosicion(lista, elemento) {
+//     let indice = 0;
+
+//     while (indice < lista.length) {
+//         if (lista[indice] === elemento) {
+//             return indice;
+//         }
+//         indice++;
+//     }
+
+//     return -1;
+// }
+
+// let numerosx = [3, 5, 1, 9, 2, 8];
+// let elemento = 9;
+// let posicion = encontrarPosicion(numerosx, elemento);
+// console.log("La posición del elemento es:", posicion);
+
+// // Crea una función que reciba dos listas de números del mismo tamaño y
+// // devuelva una nueva lista con la suma de los elementos uno a uno.
+
+// function sumarListas(lista1, lista2) {
+//     if (lista1.length !== lista2.length) {
+//         console.log("Las listas no tienen el mismo tamaño.");
+//         return [];
+//     }
+
+//     let resultado = [];
+//     let i = 0;
+
+//     while (i < lista1.length) {
+//         resultado.push(lista1[i] + lista2[i]);
+//         i++;
+//     }
+
+//     return resultado;
+// }
+
+// let lista1 = [1, 2, 3, 4];
+// let lista2 = [5, 6, 7, 8];
+// let sumaListas = sumarListas(lista1, lista2);
+// console.log("La lista resultante es:", sumaListas);
+
+// // Crea una función que reciba una lista de números y devuelva una nueva 
+// // lista con el cuadrado de cada número.
+
+// function cuadradoDeElementos(lista) {
+//     let resultado = [];
+//     let i = 0;
+
+//     while (i < lista.length) {
+//         resultado.push(lista[i] ** 2);
+//         i++;
+//     }
+
+//     return resultado;
+// }
+
+
+// let numeros = [1, 2, 3, 4, 5];
+// const cuadrados = cuadradoDeElementos(numeros);
+// console.log("La lista de cuadrados es:", cuadrados);
